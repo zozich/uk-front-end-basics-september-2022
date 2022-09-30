19c2c07455728b01febae5519ace60721d8b68e2
Andrew Koziulia <zozich2010@gmail.com>
2022-09-30 22:59:38 +0300

    Third webinar, CSS and JS

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// alert("Hello world!")
+
+console.log("First message to console");
+
+console.log($(".section-control"))
+console.log($(".restaurant"))
+
+// $(".section-control").click(function () {
+//   $(".slider").slideToggle()
+// })
+
+$('.slider').slick({
+  infinite: true,
+  slidesToShow: 4,
+});
